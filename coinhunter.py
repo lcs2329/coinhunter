aa6fa9f53cf5c69262c8ce7ced3fe2f39ac75eff
nourishablegecko <lukesturgis227@gmail.com>
2019-11-18 10:22:34 -0500

    fixed threading and some other minor issues

--- coinhunter.py
+++ coinhunter.py
@@ -5,29 +5,57 @@ import logging
 import coloredlogs
 import argparse
 from bs4 import BeautifulSoup
+import json
 from urllib.parse import urljoin
+import re
+import threading
 from concurrent.futures import ThreadPoolExecutor
 
 log = logging.getLogger(__name__)
 coloredlogs.install(level="INFO", fmt="%(message)s", logger=log)
 
-urls_to_parse = []
+urls_to_parse = {}
 known_urls = []
 
+
 def get_sublinks(base_url, depth):
+    if depth >= args.depth:
+        return
+
     log.debug(f"DEPTH: {depth}: Querying {base_url} for sublinks...")
 
-    response = requests.get(base_url)
+    response = requests.get(base_url, timeout=10)
+    if response.status_code != 200:
+        log.error(f"Url {base_url} returned {response.status_code}")
+        return
+
     soup = BeautifulSoup(response.text, features="lxml")
+
+    remote_scripts = []
+    for script in soup.find_all("script"):
+        if "src" in script.attrs.keys():
+            absolute_url = urljoin(base_url, script.attrs["src"])
+            log.debug(f"Remote script found, src = {absolute_url}")
+        else:
+            if "coinhive" in script.text.lower():
+                log.warning(f"Found coinhive script: \nSource: {base_url} \nContents:\n{script}")
 
     for link in soup.find_all("a", href=True):
 
         absolute_url = urljoin(base_url, link["href"]) 
-        if absolute_url not in known_urls:
-            known_urls.append(absolute_url)
+        if absolute_url not in known_urls and absolute_url not in urls_to_parse.keys():
+            log.debug(f"Adding {absolute_url} to links to process...")
+            urls_to_parse[absolute_url] = depth + 1
 
 
-
+def watch_for_links():
+    log.debug("Spawning base thread to watch for URLs...")
+    while True:
+        if len(urls_to_parse) > 0:
+            for link in list(urls_to_parse):
+                pool.submit(get_sublinks, link, urls_to_parse[link])
+                known_urls.append(link)
+                del urls_to_parse[link]
 
 
 parser = argparse.ArgumentParser()
@@ -44,10 +72,12 @@ if args.verbose:
     )
 
 pool = ThreadPoolExecutor(args.threads)
-
+t = threading.Thread(target=watch_for_links)
+t.start()
 
 base_url = args.url
 if not base_url.startswith("http://") or not base_url.startswith("https://"):
     base_url = "http://" + base_url
 
-get_sublinks(base_url, args.depth)
+#get_sublinks(base_url, 0)
+urls_to_parse[base_url] = 0
